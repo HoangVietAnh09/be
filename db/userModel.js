8e6962cdcb8126c4e0a9fbf8dc5a731017d71896
HoangVietAnh09 <vietanh092004@gmail.com>
2025-12-25 08:55:12 +0700

    Update

--- db/userModel.js
+++ db/userModel.js
@@ -8,6 +8,12 @@ const userSchema = new mongoose.Schema({
   location: { type: String },
   description: { type: String },
   occupation: { type: String },
+  // friend: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "User",
+  //   },
+  // ],
 });
 
 module.exports = mongoose.model.Users || mongoose.model("Users", userSchema);
